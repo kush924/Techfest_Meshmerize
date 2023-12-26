8927e60e1e46880e699fa54d0793132844e08725
Pranav Gupta <kush924@gmail.com>
2023-12-26 07:20:19 +0530

    update

--- Master/Master_Techfest_3_0_1/turns_dist.ino
+++ Master/Master_Techfest_3_0_1/turns_dist.ino
@@ -2,7 +2,7 @@ void trigCheckDist(){
   if(led_line_logic[0][led_arr_trig_lr] || led_line_logic[6][led_arr_trig_lr] == 1){
   //trigger
   }
-  if(led_line_logic[3][led_arr_trig_f]){
+  if(led_line_logic[3][led_arr_trig_f]==0){
     //trigger
   }
 }
